b0da93f2eb13062081ae2a9baeca47cdb28d38c1
Stanislav Berestetskiy <stasberestetskiy@gmail.com>
2023-02-17 15:19:42 +0100

    greeting section is done along with media queries

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const navbarToggle = document.querySelector('.header-toggle');
 const navbarLinks = document.querySelector('.menu');
 const navbarItems = document.querySelectorAll('.header-item');
@@ -5,9 +6,13 @@ const navbarItems = document.querySelectorAll('.header-item');
 navbarItems.forEach((element) => {
 	element.addEventListener('click', () => {
 		navbarLinks.classList.toggle('menu-active');
+
+		body.style.overflow = 'auto';
 	});
 });
 
 navbarToggle.addEventListener('click', () => {
 	navbarLinks.classList.toggle('menu-active');
+
+	body.style.overflow = 'hidden';
 });
